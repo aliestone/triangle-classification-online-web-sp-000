6575b349b92ced0399e1e5c171acf76167cb58b6
Alie Stone <helloaliestone@gmail.com>
2020-10-09 13:24:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+attr_accessor :isosceles, :equilateral, :scalene
+
+class TriangleError < StandardError
+  end 
+end 
+
+  def initialization(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def equilateral
+    a == b == c
+  end
+    
+  end 
+
+  def kind
+  end 
+
+
+  
+
+  
+
+
 end
