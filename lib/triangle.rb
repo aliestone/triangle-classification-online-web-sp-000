4dfd6d4f2cf54ae977a8eb7ce40ed34b0de713be
Alie Stone <helloaliestone@gmail.com>
2020-10-09 13:26:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,18 @@ end
     @c = c
   end
 
-  def equilateral
-    a == b == c
+  def kind (a, b, c)
+    a, b, c = [a, b, c].sort
+    raise TriangleError if a <= 0 or a + b <= c
+    return :equilateral if a == c
+    return :isosceles if a == b or b == c
+    return :scalene
+    end
   end
+end
 
-  end
+end
 
-  def kind
-  end
 
 
 
